157d4d3ce8f9220623b7d1d914cdf2ce6bbfb70d
Tapendra Shahi <tapendrashahi61@gmail.com>
2026-03-31 14:13:59 +0545

    Ready for deployment.

---
+++ backend/utils/createToken.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  // Set JWT as an HTTP-Only Cookie
+  const isProduction = process.env.NODE_ENV === "production";
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "strict",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+
+  return token;
+};
+
+export default generateToken;
