727f6f84f15630e1cb09657ef1520297a6684a20
Tapendra Shahi <tapendrashahi61@gmail.com>
2026-03-31 16:40:32 +0545

    Image upload fix

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,12 +1,18 @@
 import path from "path";
+import fs from "fs";
 import express from "express";
 import multer from "multer";
 
 const router = express.Router();
 
+const uploadDir = "uploads/";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
 
   filename: (req, file, cb) => {
